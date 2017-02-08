48100d89750f1367195b1f247ae18d8fb1575545
jiangpeiran <jiangpeiran@meituan.com>
2017-02-08 16:40:13 +0800

    add interface for boss.cang

---
+++ src/main/java/com/cbtsoft/pokercenter/helper/GoldenFlowerComparator.java
@@ -0,0 +1,28 @@
+package com.cbtsoft.pokercenter.helper;
+
+import com.cbtsoft.pokercenter.model.Card;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class GoldenFlowerComparator implements Comparator<List<Card>> {
+
+    /**
+     * @param cardList Cards show on the desk.
+     */
+    public GoldenFlowerComparator(List<Card> cardList) {
+        // TODO: boss cang, add your code here.
+    }
+
+    /**
+     *
+     * @param o1 Cards from player one
+     * @param o2 Cards from player two
+     * @return
+     */
+    @Override
+    public int compare(List<Card> o1, List<Card> o2) {
+        // TODO: boss cang, add your code here.
+        return 0;
+    }
+}
---
+++ src/main/java/com/cbtsoft/pokercenter/model/Deck.java
@@ -0,0 +1,20 @@
+package com.cbtsoft.pokercenter.model;
+
+import java.util.List;
+
+public interface Deck {
+    /**
+     * Deal one card from the deck.
+     *
+     * @return one card
+     */
+    Card deal();
+
+    /**
+     * Deal n cards from the deck.
+     *
+     * @param n
+     * @return n cards
+     */
+    List<Card> deal(int n);
+}
